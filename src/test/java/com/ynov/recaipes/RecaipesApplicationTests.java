1ee6aab496b8381b55d9aad8aad33e979f1910bd
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2025-05-20 10:14:30 +0200

    initial commit

---
+++ src/test/java/com/ynov/recaipes/RecaipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.recaipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
